528d36743b7461d205dbb6c513c8355e9acca6f6
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-13 09:54:01 +0100

    correct part1

--- obs_introprog_test3.java
+++ obs_introprog_test3.java
@@ -2,7 +2,7 @@ class Test3 {
     
     public static void main (String[] args){
         //Part 1
-        sum(3, 79, 12);
+        System.out.println(sum(3, 79, 12));
 
         //Part 2
         Coche miCoche = new Coche();
@@ -13,9 +13,8 @@ class Test3 {
     }
 
     //Part 1
-    public static void sum (int a, int b, int c){
-        int d = a + b + c;
-        System.out.println(d);
+    public static int sum (int a, int b, int c){
+        return a + b + c;
     }
 
 }
