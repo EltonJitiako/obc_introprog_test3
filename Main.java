39b7151abd0e11da0f25a40d58c5cce6f9a249cd
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-13 10:19:48 +0100

    correct Main

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+    
+    public static void main (String[] args){
+        //Part 1
+        System.out.println(sum(3, 79, 12));
+
+        //Part 2
+        Coche miCoche = new Coche();
+        miCoche.masPuertas();
+        System.out.println(miCoche.puertas);
+
+    
+    }
+
+    //Part 1
+    public static int sum (int a, int b, int c){
+        return a + b + c;
+    }
+
+}
+
+
+//Part 2
+class Coche {
+    int puertas = 4;
+    void masPuertas() {
+        puertas +=1;
+    }
+}
